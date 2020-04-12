a77dc5212024a258a413cd3d8803258c41e8fc0e
moh-hit <moh.hit1012@gmail.com>
2020-04-12 20:52:09 +0530

    Updates

---
+++ precache-manifest.37b6958df55484ba8fcd3916b0031045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af84cd71cf5660b0465b8d794557ec3",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "e7f63a46d1a7c54c50c3",
+    "url": "/covid19tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "445d5917ff6039261b23",
+    "url": "/covid19tracker/static/css/main.2b7a9898.chunk.css"
+  },
+  {
+    "revision": "e7f63a46d1a7c54c50c3",
+    "url": "/covid19tracker/static/js/2.6eb50c8c.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19tracker/static/js/2.6eb50c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445d5917ff6039261b23",
+    "url": "/covid19tracker/static/js/main.5baae6fb.chunk.js"
+  },
+  {
+    "revision": "a2db24feb3f46cbd9e15",
+    "url": "/covid19tracker/static/js/runtime-main.07cfe111.js"
+  }
+]);
